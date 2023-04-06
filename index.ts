1a28efd79df7f79b9829b305d08dad024a7383dd
Austin Wang <yyaustin@bu.edu>
2023-04-06 06:33:48 -0400

    Finish exercise 9.5

--- index.ts
+++ index.ts
@@ -1,9 +1,27 @@
 import express from "express"
+import { isNotNumber } from "./utils/isNotNumber"
 
 const app = express()
 
 app.get("/", (_req, res) => {
   res.send("Hello")
+})
+
+app.get("/bmi", (req, res) => {
+  const height = Number(req.query.height)
+  const weight = Number(req.query.weight)
+  if (isNotNumber(height) || isNotNumber(weight)) {
+    res.json({
+      error: "malformatted parameters"
+    })
+  }
+
+  const bmi = weight / ((height / 100) ** 2)
+
+  res.json({
+    weight, height,
+    bmi: `Your BMI is: ${bmi}`
+  })
 })
 
 const PORT = 3003
